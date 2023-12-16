6ed8cf36b8ef19a878be6a6fe72c70f10e8ef11f
Krishan Murari <144571603+SShayar@users.noreply.github.com>
2023-12-16 13:00:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,43 @@ function getMSFromProperty(property,selector) {
 //     document.querySelector("body,nav").classList.toggle("dark");
 // })
 
+
+var aText = new Array(
+	"Hi Viewer, My self Krishan",
+	"As a web developer, I thrive on turning lines of code into beautifully functional websites, infusing each project with a touch of innovation.",
+	"creating something is the Best felling ever it's feels like u are a Crafter & creater of Digital world", 
+	);
+	var iSpeed = 100; // time delay of print out
+	var iIndex = 0; // start printing array at this posision
+	var iArrLength = aText[0].length; // the length of the text array
+	var iScrollAt = 20; // start scrolling up at this many lines
+	 
+	var iTextPos = 0; // initialise text position
+	var sContents = ''; // initialise contents variable
+	var iRow; // initialise current row
+	 
+	function typewriter()
+	{
+	 sContents =  ' ';
+	 iRow = Math.max(0, iIndex-iScrollAt);
+	 var destination = document.getElementById("typedtext");
+	 
+	 while ( iRow < iIndex ) {
+	  sContents += aText[iRow++] + '<br />';
+	 }
+	 destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "🖋️";
+	 if ( iTextPos++ == iArrLength ) {
+	  iTextPos = 0;
+	  iIndex++;
+	  if ( iIndex != aText.length ) {
+	   iArrLength = aText[iIndex].length;
+	   setTimeout("typewriter()", 500);
+	  }
+	 } else {
+	  setTimeout("typewriter()", iSpeed);
+	 }
+	}
+	typewriter();
+
+
+	
